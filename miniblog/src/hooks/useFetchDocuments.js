233989cf96def1cd5d405a5cda83eb846272a247
Eros Netto <erosnetto1002@gmail.com>
2023-08-18 10:52:46 -0300

    Post individual feito

--- miniblog/src/hooks/useFetchDocuments.js
+++ miniblog/src/hooks/useFetchDocuments.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { db } from "../firebase/config";
-import { collection, query, orderBy, onSnapshot, where, QuerySnapshot } from "firebase/firestore";
+import { collection, query, orderBy, onSnapshot, where } from "firebase/firestore";
 
 export const useFetchDocuments = (docCollection, search = null, uid = null) => {
   const [documents, setDocuments] = useState(null);
--- miniblog/src/pages/Post/Post.jsx
+++ miniblog/src/pages/Post/Post.jsx
@@ -2,13 +2,28 @@ import styles from './Post.module.css';
 
 // Hooks
 import { useParams } from 'react-router-dom';
+import { useFetchDocument } from '../../hooks/useFetchDocument';
 
 const Post = () => {
   const { id } = useParams();
+  const { document: post, loading } = useFetchDocument('posts', id);
 
   return (
-    <div>
-      <h1>Post {id}</h1>
+    <div className={styles.post_container}>
+      {loading && <p>Carregando post...</p>}
+      {post && (
+        <>
+          <h1>{post.title}</h1>
+          <img src={post.image} alt={post.title} />
+          <p>{post.body}</p>
+          <h3>Este post trata sobre:</h3>
+          <div className={styles.tags}>
+            {post.tagsArray.map((tag) => (
+              <p key={tag}><span>#</span>{tag}</p>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
