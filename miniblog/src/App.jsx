85ad4e141bc9cb857a0d54e4c7cb895bd01d8577
Eros Netto <erosnetto1002@gmail.com>
2024-01-26 15:41:55 -0300

    Rotas do app

--- miniblog/src/App.jsx
+++ miniblog/src/App.jsx
@@ -1,29 +1,29 @@
-import './App.css';
+import "./App.css";
 // eslint-disable-next-line
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { onAuthStateChanged } from 'firebase/auth';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
 
 // Hooks
-import { useState, useEffect } from 'react';
-import { useAuthentication } from './hooks/useAuthentication';
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
 
 // Components
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 // Context
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider } from "./contexts/AuthContext";
 
 // Pages
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
-import CreatePost from './pages/CreatePost/CreatePost';
-import Dashboard from './pages/Dashboard/Dashboard';
-import Search from './pages/Search/Search';
-import Post from './pages/Post/Post';
-import EditPost from './pages/EditPost/EditPost';
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Search from "./pages/Search/Search";
+import Post from "./pages/Post/Post";
+import EditPost from "./pages/EditPost/EditPost";
 
 function App() {
   const [user, setUser] = useState(undefined);
@@ -48,15 +48,30 @@ function App() {
           <NavBar />
           <div className="container">
             <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/search' element={<Search />} />
-              <Route path='/posts/:id' element={<Post />} />
-              <Route path='/login' element={!user ? <Login /> : <Navigate to='/' />} />
-              <Route path='/register' element={!user ? <Register /> : <Navigate to='/' />} />
-              <Route path='/posts/edit/:id' element={user ? <EditPost /> : <Navigate to='/login' />} />
-              <Route path='/posts/create' element={user ? <CreatePost /> : <Navigate to='/login' />} />
-              <Route path='/dashboard' element={user ? <Dashboard /> : <Navigate to='/login' />} />
+              <Route path={["/", "/Projeto-MiniBlog"]} element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/posts/:id" element={<Post />} />
+              <Route
+                path="/login"
+                element={!user ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/register"
+                element={!user ? <Register /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/posts/edit/:id"
+                element={user ? <EditPost /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/posts/create"
+                element={user ? <CreatePost /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/dashboard"
+                element={user ? <Dashboard /> : <Navigate to="/login" />}
+              />
             </Routes>
           </div>
           <Footer />
