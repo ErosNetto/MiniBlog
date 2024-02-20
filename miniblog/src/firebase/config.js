e535639852529980d851ecf5102e16bc6dbb5ae9
Eros Netto Antunes <113219307+ErosNetto@users.noreply.github.com>
2024-02-20 18:35:31 -0300

    Update config.js

--- miniblog/src/firebase/config.js
+++ miniblog/src/firebase/config.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2ZiAo3pln4JWNBzdnJUuOgFVOdrDoQvc",
-  authDomain: "miniblog-b9b64.firebaseapp.com",
-  projectId: "miniblog-b9b64",
-  storageBucket: "miniblog-b9b64.appspot.com",
-  messagingSenderId: "506273815733",
-  appId: "1:506273815733:web:414a6de24df3b9257a32fc",
+  // CONFIG FIREBASE
 };
 
 const app = initializeApp(firebaseConfig);
