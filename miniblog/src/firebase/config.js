b6285cf503b6fdb0d94b6083e010c77284f6d473
Eros Netto Antunes <erosnetto1002@gmail.com>
2024-10-29 12:02:26 -0300

    Deploy

--- miniblog/src/firebase/config.js
+++ miniblog/src/firebase/config.js
@@ -1,12 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// CONFIG FIREBASE
 const firebaseConfig = {
-  // CONFIG FIREBASE
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 
 export { db };
